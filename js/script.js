68ddb4894007c093a4c182c67a9a29d7f30e7d5e
Vahagn <boxgold014@gmail.com>
2020-08-11 13:00:45 +0400

    first commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-console.log(process);
+$(document).ready(function() {
+    let burger = $(".navbar__burger");
+    let navbar = $("#navbar");
+    $(burger).on('click', e => {
+        $(navbar).toggleClass('active')
+
+    })
+
+});
